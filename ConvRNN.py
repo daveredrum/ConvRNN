38e646ab9578a227804fac727bd067af4a912098
Dave Chen <zhenyu.chen@campus.lmu.de>
2017-12-16 16:13:34 +0100

    small changes

--- ConvRNN.py
+++ ConvRNN.py
@@ -1,9 +1,8 @@
-import tensorflow as tf
-
-class ConvLSTM:
-    def __init__(self, filter_size, filter_stride, pool_size, pool_stride, name="ConvLSTMCell"):
+ass ConvLSTM:
+    def __init__(self, input_size, filter_size, filter_stride, pool_size, pool_stride, name="ConvLSTMCell"):
         # filter_size is a dict of which the keys are layer numbers and 
         # the values are 4-D lists
+        self.input_size = input_size
         self.filter_size = filter_size
         self.filter_stride = filter_stride
         self.conv_layers = len(filter_size.keys())
@@ -12,22 +11,22 @@ class ConvLSTM:
         self.name = name
         self.weight = {}
         self.bias = {}
+        _width = self.input_size[1]
+        _height = self.input_size[2]
         for key in filter_size.keys():
             self.weight[key] = self._weight_variable(filter_size[key])
             self.bias[key] = self._bias_variable([filter_size[key][-1]])
+            _width =  _width // (self.filter_stride[1] * self.pool_stride[1])
+            _height = _height // (self.filter_stride[2] * self.pool_stride[2])
+        self.conv_output = _width * _height * self.filter_size[self.conv_layers][-1]
+        self.lstm = tf.contrib.rnn.BasicLSTMCell(self.conv_output)
             
-    def __call__(self, inputs, state=None):
+    def __call__(self, inputs, state):
         with tf.variable_scope(self.name):
             conv_output = self._conv(inputs)
             conv_shape = conv_output.get_shape().as_list()
             flattened = tf.reshape(conv_output, [conv_shape[0], -1])
-            flattened_size = flattened.get_shape().as_list()
-            if state == None:
-                self.cell = tf.contrib.rnn.BasicLSTMCell(flattened_size[-1])
-                state = (tf.zeros([conv_shape[0], flattened_size[-1]]), 
-                         tf.zeros([conv_shape[0], flattened_size[-1]]))
-            lstm_output, state = self.cell(flattened, state)
-            outputs = tf.reshape(lstm_output, conv_shape)
+            outputs, state = self.lstm(flattened, state)
             return outputs, state
             
     def _weight_variable(self, shape):
