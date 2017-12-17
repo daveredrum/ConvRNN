d96484ca88b5d2938f6d15fdbea2fd90d287715c
Dave Chen <zhenyu.chen@campus.lmu.de>
2017-12-17 12:59:23 +0100

    added ConvGRU

--- ConvRNN.py
+++ ConvRNN.py
@@ -71,3 +71,69 @@ class ConvLSTM:
         variables['wo'] = tf.Variable(tf.truncated_normal([1, self.conv_output], stddev=0.1))
         variables['bo'] = tf.Variable(tf.constant(0.1, shape=[1, self.conv_output]))
         return variables
+
+
+class ConvGRU:
+    def __init__(self, input_size, filter_size, filter_stride, pool_size, pool_stride, name="ConvGRUCell"):
+        # filter_size is a dict of which the keys are layer numbers and 
+        # the values are 4-D lists
+        self.input_size = input_size
+        self.filter_size = filter_size
+        self.filter_stride = filter_stride
+        self.conv_layers = len(filter_size.keys())
+        self.pool_size = pool_size
+        self.pool_stride = pool_stride
+        self.name = name
+        self.weight = {}
+        self.bias = {}
+        _width = self.input_size[1]
+        _height = self.input_size[2]
+        for key in filter_size.keys():
+            self.weight[key] = self._weight_variable(filter_size[key])
+            self.bias[key] = self._bias_variable([filter_size[key][-1]])
+            _width =  _width // (self.filter_stride[1] * self.pool_stride[1])
+            _height = _height // (self.filter_stride[2] * self.pool_stride[2])
+        self.conv_output = _width * _height * self.filter_size[self.conv_layers][-1]
+        self.gru_variable = self._initialize_lstm()
+            
+    def __call__(self, inputs, state):
+        with tf.variable_scope(self.name):
+            conv_output = self._conv(inputs)
+            conv_shape = conv_output.get_shape().as_list()
+            flattened = tf.reshape(conv_output, [conv_shape[0], -1])
+            outputs, state = self._gru(flattened, state)
+            return outputs, state
+            
+    def _weight_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def _bias_variable(self, shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+    
+    def _conv(self, inputs):
+        for layer in self.filter_size.keys():
+            inputs = tf.nn.conv2d(
+                inputs, self.weight[layer], self.filter_stride, padding="SAME") + self.bias[layer]
+            inputs = tf.nn.relu(inputs)
+            inputs = tf.nn.max_pool(inputs, self.pool_size, self.pool_stride, padding="SAME")
+        return inputs
+    
+    def _gru(self, inputs, state):
+        h = state
+        z = tf.sigmoid(tf.matmul((h + inputs), self.gru_variable['wz']))
+        r = tf.sigmoid(tf.matmul((h + inputs), self.gru_variable['wr']))
+        h_tilde = tf.tanh(tf.multiply(self.gru_variable['w'], (r * h) + inputs))
+        h = (1 - z) * h + z * h_tilde
+        return h, h
+    
+    def initState(self):
+        return tf.zeros([self.input_size[0], self.conv_output])
+    
+    def _initialize_lstm(self):
+        variables = {}
+        variables['wz'] = tf.Variable(tf.truncated_normal([self.conv_output, self.input_size[0]], stddev=0.1))
+        variables['wr'] = tf.Variable(tf.truncated_normal([self.conv_output, self.input_size[0]], stddev=0.1))
+        variables['w'] = tf.Variable(tf.truncated_normal([self.input_size[0], self.conv_output], stddev=0.1))
+        return variables
